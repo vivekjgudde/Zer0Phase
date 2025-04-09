d82b53100da5a6cd264418ba093ac4b3d766f280
jhansi <jhansi12mar2000@gmail.com>
2025-04-09 10:56:56 +0530

    loops syntax

---
+++ loops.sh
@@ -0,0 +1,31 @@
+for loop
+for var in item1 item2 item3; do
+  commands
+done
+
+for i in {1..5}; do
+  echo "Number $i"
+done
+
+while loop
+while [ condition ]; do
+  commands
+done
+
+break and continue
+for i in {1..10}; do
+  if [ $i -eq 5 ]; then
+    break
+  fi
+  echo "i = $i"
+done
+
+example
+while true; do
+  read -p "Enter your name (or 'q' to quit): " name
+  if [ "$name" = "q" ]; then
+    break
+  fi
+  echo "Hello, $name!"
+done
+
