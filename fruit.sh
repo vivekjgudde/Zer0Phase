53db80bc6d98ec0720754f93e91635f8ebb8c5b7
Maddy0208 <Madhugn2597@gmail.com>
2025-04-09 07:57:50 +0530

    Git Practice
    Linux and GIt practice files

---
+++ fruit.sh
@@ -0,0 +1,14 @@
+#! /bin/bash 
+fruit="$1"
+village="Mysore"
+Weight="$2"
+
+echo Good Morning.
+echo " This $fruit is from $village and weighs $2 Kgs"
+
+echo name of this script is $0
+echo total number of afrguments passed to this script is $#
+PID of this Process is echo $$
+echo PID of last process that went to background is $!
+echo total number of arguments passed to this script in Array format is $@
+echo total number of arguments paseed to this script in string format is $*
